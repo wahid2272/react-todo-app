05f6bc180849f458f3a023d172dc607023e862e2
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-05-27 16:33:48 +0300

    application structure created with placeholder data and designed with css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-      </header> */}
-      <h1>To Do List</h1>
-    </div>
+      <div className="ToDo-App">
+        <h1>To Do List</h1>
+        <form autoComplete="off">
+          <input
+            type="text"
+            name="todos"
+            id="todos"
+            required
+            placeholder="Make your to-do list"
+          />
+          <button type="submit">Create</button>
+        </form>
+
+        <ul>
+          <li>
+            <label htmlFor="">
+              <input type="checkbox" name="" id="" />
+              Things to do
+            </label>
+            <button>Edit</button>
+          </li>
+          <li>
+            <label htmlFor="">
+              <input type="checkbox" name="" id="" />
+              Things to do
+            </label>
+            <button>Edit</button>
+          </li>
+        </ul>
+
+        <div className="row">
+          <label htmlFor="all">
+            <input type="checkbox" name="all" id="all" />
+            All Complete
+          </label>
+          <p>You have 0 things to do</p>
+          <button id="delete">Delete All</button>
+        </div>
+      </div>
   );
 }
 
