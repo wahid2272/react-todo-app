bc010938388450d3ca1da595c42f13e47758e548
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-05-30 17:12:56 +0300

    forminput component restructured with hooks

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {DataContext} from './context/DataContext';
 
 const FormInput = () => {
+  const [todos, setTodos] = useContext(DataContext);
+  const [todoName, setTodoName] = useState('');
+
+  const addTodo = e =>{
+    e.preventDefault();
+    setTodos([...todos, {name: todoName, complete: false}])
+    setTodoName('');
+  }
+
   return (
     <div>
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={addTodo}>
           <input
             type="text"
             name="todos"
             id="todos"
             required
             placeholder="Make your to-do list"
+            value={todoName}
+            onChange={e => setTodoName(e.target.value)}
           />
           <button type="submit">Create</button>
         </form>
