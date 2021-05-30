ddf7cb967cc2411a1f565211a2df9e9eb0e0e3d1
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-05-30 19:00:04 +0300

    Header section with navber added in the application, designed with material ui

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import List from './components/tasks/List';
 import Footer from './components/footer/Footer';
 import {DataContextProvider} from './components/context/DataProvider';
 
+// Material UI
+import Header from './components/header/Header';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 function App() {
   return (
-    <DataContextProvider>
-      <div className="ToDo-App">
-        <h1>To Do List</h1>
-        <FormInput />
-        <List />
-        <Footer />
-      </div>
-    </DataContextProvider>
+    <div>
+      <CssBaseline />
+      <Header/>
+      <DataContextProvider>
+        <div className="ToDo-App">
+          <h1>To Do List</h1>
+          <FormInput />
+          <List />
+          <Footer />
+        </div>
+      </DataContextProvider>
+    </div>
   );
 }
 
