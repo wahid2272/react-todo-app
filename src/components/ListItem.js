2431dad31537e2ac57483bb0276e0ee839f7dfe2
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-05-30 18:16:11 +0300

    edit functinality of created tasks is added to the app

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const ListItem = ({todo, id, checkCompleteStatus}) => {
-  return (
-    <div>
-      <li>
-        <label htmlFor={id} className={todo.complete ? "active" : ""} >
-          <input type="checkbox" id={id} checked={todo.complete} onChange={() => checkCompleteStatus(id)} />
-          {todo.name}
-        </label>
-        <button disabled={todo.complete}>Edit</button>
-      </li>
-    </div>
-  );
+const ListItem = ({todo, id, checkCompleteStatus, handleEditTodos}) => {
+  const [edit, setEdit] = useState(false);
+  const [editValue, setEditValue] = useState(todo.name)
+
+  const handleEdit = () => {
+    setEdit(true);
+  }
+
+  const handleConfirm = id => {
+    setEdit(false);
+    if(editValue){
+      handleEditTodos(editValue,id)
+    }else{
+        setEditValue(todo.name)
+    }
+  }
+
+  if(edit) {
+    return (
+      <div>
+        <li>
+          <input type="text" id={editValue} value={editValue} name={editValue} onChange={e => setEditValue(e.target.value)}/>
+          <button onClick={() => handleConfirm(id)}>Confirm</button>
+        </li>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        <li>
+          <label htmlFor={id} className={todo.complete ? "active" : ""} >
+            <input type="checkbox" id={id} checked={todo.complete} onChange={() => checkCompleteStatus(id)} />
+            {todo.name}
+          </label>
+          <button disabled={todo.complete} onClick={handleEdit} >Edit</button>
+        </li>
+      </div>
+    );
+  }
 };
 
 export default ListItem;
